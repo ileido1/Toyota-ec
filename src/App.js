4de3d1c98004925bec3f3aba73b5910fb18fd48e
ileido1 <soycarlosluis862@gmail.com>
2022-06-22 15:20:03 -0400

    Footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './css/bootstrap.min.css';
 import './css/style.css';
 import Header from './Header'
 import Cart from './Cart'
+import Footer from './Footer';
 
-const App = () => ( 
-    <>
-      <Header />
-      {window.location.pathname === "/cart" &&  <Cart /> }
-    </>
+const App = () => (
+  <>
+    <Header />
+    {window.location.pathname === "/cart" && <Cart />}
+    <Footer></Footer>
+  </>
 )
 
 export default App;
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+        <footer>
+            <div id="footer" className="container-fluid">
+                <div className="row">
+                    <img className="img-footer" src="http://toyota-site.247.com.ec/wp-content/themes/twentyten/img/foot.png" alt="" ></img>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
+
