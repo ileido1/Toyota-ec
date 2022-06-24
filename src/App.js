dc059f659f4626a931fe4b2a9706a5ac45ca5d82
ileido1 <soycarlosluis862@gmail.com>
2022-06-24 12:55:49 -0400

    urd

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import './css/bootstrap.min.css';
+
+
+import './css/bootstrap.min.css'
 import './css/style.css';
 import Header from './Header'
 import Cart from './Cart'
 import Footer from './Footer';
+
 import Baneer from './Banner'
+import Urd from './urd';
 
 const App = () => (
   <>
     <Header />
     {window.location.pathname === "/cart" && <Cart />}
     {window.location.pathname === "/" && <Baneer />}
+    <Urd></Urd>
     <Footer></Footer>
   </>
 )
--- src/Cart.js
+++ src/Cart.js
@@ -46,10 +46,10 @@ const Cart = () => (
             </div>
 
             <div className="row">
-            
-            <div className="col-6">
-                <div className="content_features" id="content-features" >
-                <div className="row">
+
+                <div className="col-6">
+                    <div className="content_features" id="content-features" >
+                        <div className="row">
                             <div className="col-6 col-sm-6 border-right-filled">
                                 <div className="row">
                                     <div className="col-12">
@@ -60,75 +60,75 @@ const Cart = () => (
 
                                     <div className="col-4 animated fadeInLeft delay3 duration4">
                                         <img className="img-icono-feature" src={hac} alt="Hac" />
-                                        
+
                                     </div>
                                     <div className="col-4 animated fadeInLeft delay3 duration4">
                                         <img className="img-icono-feature-frenos" src={frenos} alt="Frenos" />
-                                        
+
                                     </div>
                                     <div className="col-4 animated fadeInLeft delay3 duration4">
                                         <img className="img-icono-feature-motor" src={motor} alt="Motor" />
-                                        
+
                                     </div>
 
-                                    <div className="col-4 ">                       
+                                    <div className="col-4 ">
                                         <p className="subtext-feature animated fadeInLeft delay3 duration4" >Arranque en Pendiente HAC</p>
                                     </div>
-                                    <div className="col-4 ">                                 
+                                    <div className="col-4 ">
                                         <p className="subtext-feature animated fadeInLeft delay3 duration4" >Frenos ABC + EBD</p>
                                     </div>
-                                    <div className="col-4 ">                                   
+                                    <div className="col-4 ">
                                         <p className="subtext-feature animated fadeInLeft delay3 duration4" >Motor 1.2Lt</p>
                                     </div>
 
                                 </div>
-                                
+
                             </div>
-                            
+
                             <div className="col-6">
                                 <div className="row">
                                     <div className="col-12">
-                                            <p className="titulo-feature  animated fadeInLeft delay2 duration4" > </p>
-                                            <br></br>
-                                            <br></br>
-                                           <img className="img-logo-feature  animated fadeInLeft delay3 duration4" src={logo_raize} alt="Logo Raize" /><span className="text-feature-version animated fadeInLeft delay2 duration4 " > 4X2CVT</span>
-                                            <p className="text-feature animated fadeInLeft delay4 duration4">Transmisión automática CVT Compacta, para una alta eficiencia mecánica. con 7 relaciones de cambio simuladas</p>
-                                        </div>
+                                        <p className="titulo-feature  animated fadeInLeft delay2 duration4" > </p>
+                                        <br></br>
+                                        <br></br>
+                                        <img className="img-logo-feature  animated fadeInLeft delay3 duration4" src={logo_raize} alt="Logo Raize" /><span className="text-feature-version animated fadeInLeft delay2 duration4 " > 4X2CVT</span>
+                                        <p className="text-feature animated fadeInLeft delay4 duration4">Transmisión automática CVT Compacta, para una alta eficiencia mecánica. con 7 relaciones de cambio simuladas</p>
+                                    </div>
 
-                                        <div className="col-4 animated fadeInLeft delay5 duration4">
-                                            <img className="img-icono-feature" src={hac} alt="Hac" />
-                                            
-                                        </div>
-                                        <div className="col-4 animated fadeInLeft delay5 duration4">
-                                            <img className="img-icono-feature-frenos" src={frenos} alt="Frenos" />
-                                            
-                                        </div>
-                                        <div className="col-4 animated fadeInLeft delay5 duration4">
-                                            <img className="img-icono-feature-motor" src={motor} alt="Motor" />
-                                            
-                                        </div>
+                                    <div className="col-4 animated fadeInLeft delay5 duration4">
+                                        <img className="img-icono-feature" src={hac} alt="Hac" />
 
-                                        <div className="col-4 ">                       
-                                            <p className="subtext-feature animated fadeInLeft delay5 duration4" >Arranque en Pendiente HAC</p>
-                                        </div>
-                                        <div className="col-4 ">                                 
-                                            <p className="subtext-feature animated fadeInLeft delay5 duration4" >Frenos ABC + EBD</p>
-                                        </div>
-                                        <div className="col-4 ">                                   
-                                            <p className="subtext-feature animated fadeInLeft delay5 duration4" >Motor 1.2Lt</p>
-                                        </div>
+                                    </div>
+                                    <div className="col-4 animated fadeInLeft delay5 duration4">
+                                        <img className="img-icono-feature-frenos" src={frenos} alt="Frenos" />
 
-                                    </div>                       
+                                    </div>
+                                    <div className="col-4 animated fadeInLeft delay5 duration4">
+                                        <img className="img-icono-feature-motor" src={motor} alt="Motor" />
+
+                                    </div>
+
+                                    <div className="col-4 ">
+                                        <p className="subtext-feature animated fadeInLeft delay5 duration4" >Arranque en Pendiente HAC</p>
+                                    </div>
+                                    <div className="col-4 ">
+                                        <p className="subtext-feature animated fadeInLeft delay5 duration4" >Frenos ABC + EBD</p>
+                                    </div>
+                                    <div className="col-4 ">
+                                        <p className="subtext-feature animated fadeInLeft delay5 duration4" >Motor 1.2Lt</p>
+                                    </div>
+
                                 </div>
                             </div>
                         </div>
                     </div>
-                
+                </div>
+
 
             </div>
-    
+
         </div>
-        
+
         <div className="row" id="content-diseno" >
             <div className="col-12">
                 <div id="diseno" className="row pd100"  >
@@ -138,33 +138,33 @@ const Cart = () => (
 
                             <img src={logo_raize_2} width="200px" alt="" className=" animated fadeInLeft delay2 duration4 eds-on-scroll" />
                             <p className="text-cotizar  animated fadeInLeft delay2 duration4 eds-on-scroll" > El nuevo Toyota Raize llega para expresar la energía y fuerza de quien lo conduce.</p>
-                            
+
                         </div>
 
                         <div className="content-cotizar">
-                        <a href="#" className="btn btn-cotizar  animated fadeInLeft delay2 duration4 eds-on-scroll" >COTIZAR</a>
+                            <a href="#" className="btn btn-cotizar  animated fadeInLeft delay2 duration4 eds-on-scroll" >COTIZAR</a>
                         </div>
 
                         <div className="content-descarga">
-                            <img src={documento} width="28px" className=" animated fadeInLeft delay2 duration4 eds-on-scroll" alt="" /> <span  className="text-descargar-catalogo animated fadeInLeft delay2 duration4 eds-on-scroll" >DESCARGAR CATÁLOGO</span>
+                            <img src={documento} width="28px" className=" animated fadeInLeft delay2 duration4 eds-on-scroll" alt="" /> <span className="text-descargar-catalogo animated fadeInLeft delay2 duration4 eds-on-scroll" >DESCARGAR CATÁLOGO</span>
                         </div>
-                        
+
                     </div>
                     <div className="col-8">
-                        <img id="imagen-auto-diseno" className=" img-fluid  animated fadeInLeft delay2 duration4 eds-on-scroll" src={raize_negro} alt=""  />
+                        <img id="imagen-auto-diseno" className=" img-fluid  animated fadeInLeft delay2 duration4 eds-on-scroll" src={raize_negro} alt="" />
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                        <p className="text-color-diseno secciones-subtitulos animated fadeInLeft delay2 duration4 eds-on-scroll" >COLOR</p>               
+                        <p className="text-color-diseno secciones-subtitulos animated fadeInLeft delay2 duration4 eds-on-scroll" >COLOR</p>
                     </div>
                 </div>
                 <div className="row pd50" >
                     <div className="col-2">
-                        <img className="icono-color  animated fadeInLeft delay2 duration4 eds-on-scroll" src={negro}  width="50px" alt="Negro" />
+                        <img className="icono-color  animated fadeInLeft delay2 duration4 eds-on-scroll" src={negro} width="50px" alt="Negro" />
                     </div>
                     <div className="col-2">
-                        <img className="icono-color  animated fadeInLeft delay2 duration4 eds-on-scroll" src={gris}  width="50px" alt="Gris" />
+                        <img className="icono-color  animated fadeInLeft delay2 duration4 eds-on-scroll" src={gris} width="50px" alt="Gris" />
                     </div>
                     <div className="col-2">
                         <img className="icono-color  animated fadeInLeft delay2 duration4 eds-on-scroll" src={plata} width="50px" alt="Plata" />
@@ -173,10 +173,10 @@ const Cart = () => (
                         <img className="icono-color  animated fadeInLeft delay2 duration4 eds-on-scroll" src={blanco} width="50px" alt="Blanco" />
                     </div>
                     <div className="col-2">
-                        <img className="icono-color  animated fadeInLeft delay2 duration4 eds-on-scroll" src={turquesa}  width="50px" alt="Turquesa" />
+                        <img className="icono-color  animated fadeInLeft delay2 duration4 eds-on-scroll" src={turquesa} width="50px" alt="Turquesa" />
                     </div>
                     <div className="col-2">
-                        <img className="icono-color  animated fadeInLeft delay2 duration4 eds-on-scroll"  src={rojo}   width="50px" alt="Rojo" />
+                        <img className="icono-color  animated fadeInLeft delay2 duration4 eds-on-scroll" src={rojo} width="50px" alt="Rojo" />
                     </div>
                 </div>
             </div>
@@ -184,7 +184,7 @@ const Cart = () => (
 
         <div className="row">
             <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 titulo-galeria" >
-                <p className="text-color-diseno2 secciones-subtitulos" >GALERIA RAIZE</p> 
+                <p className="text-color-diseno2 secciones-subtitulos" >GALERIA RAIZE</p>
             </div>
         </div>
 
@@ -196,44 +196,44 @@ const Cart = () => (
                     <li> <span id="automovil-interior cp" onClick={video_galeria} >INTERIOR</span> </li>
                 </ul>
             </div>
-        </div> 
+        </div>
 
         <div className="row">
-        
-        <div className="col-12 col-sm-12 content-automovil-video pestaba-cuerpo-galeria"  >
-            <div >
 
-                <video width="100%" controls >
-                    <source src={video_galeria_play} type="video/mp4" />
-                    Your browser does not support HTML video.
-                </video>
+            <div className="col-12 col-sm-12 content-automovil-video pestaba-cuerpo-galeria"  >
+                <div >
+
+                    <video width="100%" controls >
+                        <source src={video_galeria_play} type="video/mp4" />
+                        Your browser does not support HTML video.
+                    </video>
+
+                </div>
+
+            </div>
+
+            <div className="col-12 col-sm-12 content-automovil-exterior hide"  >
+
+
+
+
+
+            </div>
+
+
+            <div className="col-12 col-sm-12 content-automovil-interior hide" >
+
+
 
             </div>
 
         </div>
 
-        <div className="col-12 col-sm-12 content-automovil-exterior hide"  >
-
-
-            
-            
-
-        </div>
-
-        
-        <div className="col-12 col-sm-12 content-automovil-interior hide" >
-        
-            
-
-        </div>
-    
-    </div>
-
 
     </>
 )
 
-function video_galeria(){
+function video_galeria() {
     console.log('video_galeria');
 }
 
