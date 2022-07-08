1436ff52fdde6f2be687ecdadd672eba5e1d4f03
ileido1 <soycarlosluis862@gmail.com>
2022-07-08 13:01:54 -0400

    animations mundotoyota qdr

--- src/components/mundotoyota.js
+++ src/components/mundotoyota.js
@@ -5,10 +5,16 @@ import {
 } from "react-router-dom";
 
 import useFetch from "../hooks/useFetch";
+import { motion } from 'framer-motion'
+import { useInView } from "framer-motion";
+
 
 export default function MundoToyota() {
     let endpoint = 'home/square-categorias'
     const [info, error] = useFetch(endpoint);
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: false });
+
     return (
         <>
             <div className="container mundotoyotacontainer">
@@ -19,15 +25,30 @@ export default function MundoToyota() {
 
                                 info.map(c => (
                                     <div className="row">
-                                        <div className="col-4">
+                                        <div ref={ref} style={{
+                                            transform: isInView ? "none" : "translateX(-300px)",
+                                            opacity: isInView ? 1 : 0,
+                                            transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
+                                        }}
+
+                                            className="col-4">
                                             <img src={'https://backend-toyota.247.com.ec/' + c.imagen_square_box_1} className="mundotoyotaimgsupi"></img>
                                             <img src={'https://backend-toyota.247.com.ec/' + c.imagen_square_box_3} className="carrobanner"></img>
 
                                         </div>
-                                        <div className="col-4 centrar">
+                                        <div className="col-4 centrar" ref={ref} style={{
+                                            transform: isInView ? "none" : "translateY(300px)",
+                                            opacity: isInView ? 1 : 0,
+                                            transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
+                                        }}>
                                             <h1 className="mundotoyota">{c.title_square_box}</h1>
                                         </div>
-                                        <div className="col-4">
+                                        <div className="col-4" ref={ref} style={{
+                                            transform: isInView ? "none" : "translateX(300px)",
+                                            opacity: isInView ? 1 : 0,
+                                            transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
+                                        }}
+                                        >
                                             <img src={'https://backend-toyota.247.com.ec/' + c.imagen_square_box_2} className="mundotoyotaimgsupd"></img>
                                             <img src={'https://backend-toyota.247.com.ec/' + c.imagen_square_box_4} className="carrobanner"></img>
 
