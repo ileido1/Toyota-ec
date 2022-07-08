ae2ae041104e0345cbadb8e491c19963aabc683e
agil-webimpacto <agil@webimpacto>
2022-07-08 13:51:56 -0500

    ajustes en tecnologia v2

--- src/components/sections/cart/cart-tecnologia.js
+++ src/components/sections/cart/cart-tecnologia.js
@@ -1,15 +1,10 @@
 import React, { useRef } from "react";
 import { motion,  useInView} from "framer-motion";
 
-
 import banner_tsn from '../../../images/tecnologia/banner-tsn.png'
 import icon_hac from '../../../images/tecnologia/icon-HAC.svg'
 import icon_trc from '../../../images/tecnologia/icon-trc.svg'
 import icon_vsc from '../../../images/tecnologia/icon-vsc.svg'
-
-const sin_padding = {
-    div:{padding-left:0}
-}
 
 const variants = {
     hidden: { opacity: 0.1 },
@@ -45,40 +40,40 @@ return(
         <div className="container-fluid " id="content-galeria" >
 
                 <div className="row mt40" id="tecnologia " >
-                    <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 sin-padding-lr" >
+                    <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 sinpadding" >
                         <p className="text-color-tecnologia" >TECNOLOGIA RAIZE</p> 
                     </div>
                 </div>
 
                 <div className="row">
-                    <div className="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 sin-padding-lr"  >
+                    <div className="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 sinpadding"  >
                         <img className="img-fluid icono-tecnologia" src={icon_hac} width="120px" alt="Icono Hac" />
                     </div>
-                    <div className="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 sin-padding-lr" >
+                    <div className="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 sinpadding" >
                         <img className="img-fluid icono-tecnologia" src={icon_trc} width="100px" alt="Icono Trc" />
                     </div>
-                    <div className="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 sin-padding-lr" >
+                    <div className="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 sinpaddingr" >
                         <img className="img-fluid icono-tecnologia" src={icon_vsc} width="100px" alt="Icono Vsc" />
                     </div>
                 </div> 
 
                 <div className="row mb40" >
-                    <div className="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 sin-padding-lr" >
+                    <div className="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 sinpadding" >
                         <p className="text-tecnologia" ><strong className="fuente-black" >Asistente de Arranque HAC</strong></p>
                         <p className="text-tecnologia" >Diseñado para reducir riesgos de retroceso al momento de arrancar en pendiente.</p>
                     </div>
-                    <div className="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 sin-padding-lr" >
+                    <div className="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 sinpadding" >
                         <p className="text-tecnologia" ><strong className="fuente-black" >Control de tracción TRC</strong></p>
                         <p className="text-tecnologia">Previene la pérdida de adherencia de las ruedas, y que no patinen cuándo se acelera.</p>
                     </div>
-                    <div className="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 sin-padding-lr" >
+                    <div className="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 sinpadding" >
                         <p className="text-tecnologia" ><strong className="fuente-black" >Control electrónico de estabilidad (VSC)</strong></p>
                         <p className="text-tecnologia" >Actúa frenando individualmente las ruedas en situaciones de riesgo para evitar derrapes.</p>
                     </div>
                 </div>
                 
                 <div className="row" >
-                    <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 sin-padding-lr" style={{
+                    <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 sinpadding" style={{
           fontWeight: 300
         }} >
                     <Tecnologia>
