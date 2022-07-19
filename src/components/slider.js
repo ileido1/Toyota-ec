9cbccfed2c4059f2346db67dc3353ebab4159dd3
ileido1 <soycarlosluis862@gmail.com>
2022-07-19 12:11:55 -0400

    fix qdr srcset

--- src/components/slider.js
+++ src/components/slider.js
@@ -8,19 +8,23 @@ import "swiper/css/navigation";
 
 import { Autoplay, Pagination, Navigation } from "swiper";
 
-import imagenq  from '../images/sliderqdr/Q.png'
-import imagend  from '../images/sliderqdr/D.png'
-import imagenr  from '../images/sliderqdr/R.png'
+import imagenq from '../images/sliderqdr/Q.png'
+import imagend from '../images/sliderqdr/D.png'
+import imagenr from '../images/sliderqdr/R.png'
+import imagenqm from '../images/sliderqdr/Q-responsive.png'
+import imagendm from '../images/sliderqdr/D-responsive.png'
+import imagenrm from '../images/sliderqdr/R-responsive.png'
+
 
 import "swiper/css";
 import "swiper/css/pagination";
 
 
 export default function App() {
-    //let llenarbanner = 'home/slider-qdr'
-    //const [banner, error] = useFetch(llenarbanner);
-    return (
-        <>
+  //let llenarbanner = 'home/slider-qdr'
+  //const [banner, error] = useFetch(llenarbanner);
+  return (
+    <>
       <Swiper
         spaceBetween={30}
         centeredSlides={true}
@@ -34,25 +38,27 @@ export default function App() {
         navigation={true}
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper SliderQDR"
-      > 
-                    <SwiperSlide>
-                                                            
-                        <img className="img-fluid" src={imagenq} alt="imagen q" />
+      >
+        <SwiperSlide>
 
-                    </SwiperSlide> 
-                    <SwiperSlide>
-                                                            
-                        <img className="img-fluid" src={imagend} alt="imagen d" />
+          <img className="img-fluid" src={imagenqm} srcSet={`${imagenqm} 300w, ${imagenq} 768w, ${imagenq} 1280w, ${imagenq} 3200w`} alt="imagen q" />
 
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        
-                        <img className="img-fluid" src={imagenr} alt="imagen r" />
+        </SwiperSlide>
+        <SwiperSlide>
 
-                    </SwiperSlide> 
-                                
+          <img className="img-fluid" src={imagendm} srcSet={`${imagenqm} 300w, ${imagend} 768w, ${imagend} 1280w, ${imagend} 3200w`} alt="imagen q" />
 
-            </Swiper>
-        </>
-    );
+
+        </SwiperSlide>
+        <SwiperSlide>
+
+          <img className="img-fluid" src={imagenrm} srcSet={`${imagenr} 300w, ${imagenr} 768w, ${imagenr} 1280w, ${imagenr} 3200w`} alt="imagen q" />
+
+
+        </SwiperSlide>
+
+
+      </Swiper>
+    </>
+  );
 }
