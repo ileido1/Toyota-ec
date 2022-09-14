aeaf4d40e007c7d7a3a7e25eafaedaa463488697
ileido1 <soycarlosluis862@gmail.com>
2022-09-14 09:59:13 -0400

    enviando mensaje

--- src/components/sections/blog/paginador.js
+++ src/components/sections/blog/paginador.js
@@ -13,7 +13,7 @@ export default function Pagination({ postPerPage, currentPage, totalPages, pagin
             <nav className='paginadordiv'>
                 <ul className="pagination">
                     {pageNumbers.map(c => (
-                        <li class="page-item" onClick={() => paginate(c)} key={c}><a className={currentPage === c ? "active page-link" : "page-link"}  >{c}</a></li>
+                        <li className="page-item" onClick={() => paginate(c)} key={c}><a className={currentPage === c ? "active page-link" : "page-link"}  >{c}</a></li>
                     ))}
 
                 </ul>
